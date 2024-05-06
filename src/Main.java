f3c267a72cb3059c57e238532437bd837e95b9e1
julianthunell <123653265+julianthunell@users.noreply.github.com>
2024-05-06 10:50:43 +0200

    klar

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,9 @@ public class Main {
                 
                 //Create an input stream of the client socket
                 InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
                 System.out.println("Enter your name. Type Bye to exit");;
@@ -34,6 +37,21 @@ public class Main {
                 //to the outpust stream fo the client socket
                 message = stdIn.readLine().trim();
                 pw.println(message);
+
+                //Read data from the input stream of the cleint socket
+                System.out.println("Message returned from the server = " + br.readLine());
+
+                pw.close();
+                br.close();
+                client.close();
+
+                //Stop operation
+                if(message.equalsIgnoreCase("Bye")){
+                    break;
+                }
+            }
+            catch (IOException ie){
+                System.out.println("I/O error" + ie);
             }
         }
     }
